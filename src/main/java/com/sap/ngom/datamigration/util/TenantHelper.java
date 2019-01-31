d05de4bf9a477ec3b4e50c80d23c99035d7902ba
i332445 <vicky.chen@sap.com>
2019-01-31 13:32:56 +0800

    fix type

--- src/main/java/com/sap/ngom/datamigration/util/TenantHelper.java
+++ src/main/java/com/sap/ngom/datamigration/util/TenantHelper.java
@@ -10,7 +10,7 @@ import javax.sql.DataSource;
 import java.util.List;
 
 @Component
-public class .TenantHelper {
+public class TenantHelper {
     @Autowired
     @Qualifier("sourceDataSource")
     private DataSource sourceDataSource;
