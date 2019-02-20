cea4cfbcc6acbb35ac75e3fc57acb52ca8e0e7cd
i332445 <vicky.chen@sap.com>
2019-02-20 16:33:31 +0800

    enable h2 console

--- src/main/java/com/sap/ngom/datamigration/configuration/WebSecurityConfig.java
+++ src/main/java/com/sap/ngom/datamigration/configuration/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +12,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.headers().frameOptions().sameOrigin();
         http.formLogin().disable();
         http.csrf().disable();
     }
