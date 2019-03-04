7b800959ef11b12f23cdfcfa35496eca8d627b1f
I333750 <ray.ding@sap.com>
2019-03-04 16:13:06 +0800

    save for bill-service

--- src/main/java/com/sap/ngom/datamigration/configuration/WebSecurityConfig.java
+++ src/main/java/com/sap/ngom/datamigration/configuration/WebSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.sap.ngom.datamigration.configuration;
 
 import org.springframework.context.annotation.Configuration;
-/*
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,21 +10,11 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-       @Override
-       protected void configure(HttpSecurity http) throws Exception {
-           http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-           http.headers().frameOptions().sameOrigin();
-           http.formLogin().disable();
-           http.csrf().disable();
-       }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers().permitAll().anyRequest().permitAll().and().httpBasic();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.headers().frameOptions().sameOrigin();
         http.formLogin().disable();
         http.csrf().disable();
     }
 }
-*/
