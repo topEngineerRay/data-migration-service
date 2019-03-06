d696510200fd68b48356c3009c247167c7dc77b7
I333006 <mina.xiang@sap.com>
2019-03-06 14:20:26 +0800

    support return failed records pk(single field) in the response.

--- src/main/java/com/sap/ngom/datamigration/model/verification/TableResult.java
+++ src/main/java/com/sap/ngom/datamigration/model/verification/TableResult.java
@@ -15,6 +15,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TableResult {
     private String table;
-    private List<TenantResult> tenants;
     private Boolean dataConsistent;
+    private String primaryKey;
+    private List<TenantResult> tenants;
 }
--- src/main/java/com/sap/ngom/datamigration/model/verification/TenantResult.java
+++ src/main/java/com/sap/ngom/datamigration/model/verification/TenantResult.java
@@ -1,13 +1,18 @@
 package com.sap.ngom.datamigration.model.verification;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TenantResult {
     private String tenant;
     private CountResult countResult;
+    private List<String> inconsistentRecordsResult;
 }
