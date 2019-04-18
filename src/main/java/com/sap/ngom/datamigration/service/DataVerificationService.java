0d5e77dc752b53bd07fb46703a38f0a87cff8a7e
I333750 <ray.ding@sap.com>
2019-04-18 10:27:57 +0800

    revert

--- src/main/java/com/sap/ngom/datamigration/service/DataVerificationService.java
+++ src/main/java/com/sap/ngom/datamigration/service/DataVerificationService.java
@@ -223,7 +223,6 @@ public class DataVerificationService {
             }
         }
 
-
         log.info("Data verification is completed for table: " + tableName);
         return tableResult;
     }
