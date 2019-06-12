1f249b6dda663e11198935ca841e99343f25ec25
I333750 <ray.ding@sap.com>
2019-06-12 15:37:47 +0800

    adjust time

--- src/main/java/com/sap/ngom/datamigration/listener/ChunkExecutionListener.java
+++ src/main/java/com/sap/ngom/datamigration/listener/ChunkExecutionListener.java
@@ -5,13 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.listener.ChunkListenerSupport;
 import org.springframework.batch.core.scope.context.ChunkContext;
 
+import java.time.LocalDateTime;
+
 public class ChunkExecutionListener extends ChunkListenerSupport {
 
     private static final Logger logger = LoggerFactory.getLogger(ChunkExecutionListener.class);
 
     @Override
     public void afterChunk(ChunkContext context) {
-        logger.info("A chunk commit successfully at:" + System.currentTimeMillis());
+        logger.info("A chunk commit successfully at:" + LocalDateTime.now());
         super.afterChunk(context);
     }
 
